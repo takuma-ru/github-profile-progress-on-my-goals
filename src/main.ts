5d1d8bd40eeea3eccc46bd3428ee297580f29d4e
takuma-ru <kcattdev@gmail.com>
2022-09-22 10:39:01 +0900

    [Add] `sharp` [Update] svg to png conversion process

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,38 @@ async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
 
-  app.use(helmet());
-  app.enableCors();
+  const ga = 'https://www.google-analytics.com';
+  const gas = 'https://ssl.google-analytics.com';
+  const fbs = 'https://firebasestorage.googleapis.com';
+  const gtm = 'https://www.googletagmanager.com';
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'base-uri': ["'self'"],
+          'block-all-mixed-content': [],
+          'font-src': ["'self'", 'https:', 'data:'],
+          'form-action': ["'self'"],
+          'frame-ancestors': ["'self'"],
+          'img-src': ["'self'", 'data:', '*'],
+          'object-src': ["'none'"],
+          'script-src': ["'self'", ga, gas, gtm],
+          'script-src-attr': ["'none'"],
+          'style-src': ["'self'", 'https:', "'unsafe-inline'"],
+          'upgrade-insecure-requests': [],
+          'connect-src': ["'self'", ga],
+        },
+      },
+    }),
+  );
+  app.enableCors({
+    origin: [`http://localhost:${port}`],
+  });
 
   await app.listen(port);
 
-  console.log(`http://localhost:${port}/`);
+  console.log(`http://localhost:${port}`);
 }
 bootstrap();
